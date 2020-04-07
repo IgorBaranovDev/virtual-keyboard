23ee8145d109d91170f6c19f43693710a564535d
Baranov Igor <igor.82bv@mail.ru>
2020-04-07 18:13:41 +0300

    feat: add change language

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const body = document.querySelector('body');
 const textInput = document.createElement('textarea');
-const keyboard = document.createElement('div');
 
-keyboard.classList.add('keyboard');
 textInput.classList.add('textarea');
 body.appendChild(textInput);
-body.appendChild(keyboard);
 
 const keys = {
   eng: [
@@ -23,7 +20,7 @@ const keys = {
       '0',
       '-',
       '=',
-      'backspace'
+      'backspace',
     ],
     [
       'tab',
@@ -40,7 +37,7 @@ const keys = {
       '[',
       ']',
       '\\',
-      'delete'
+      'delete',
     ],
     [
       'capslock',
@@ -55,7 +52,7 @@ const keys = {
       'l',
       ';',
       "'",
-      'enter'
+      'enter',
     ],
     [
       'shift',
@@ -70,7 +67,7 @@ const keys = {
       '.',
       '/',
       'arrowUp',
-      'shift'
+      'shift',
     ],
     [
       'ctrl',
@@ -81,23 +78,243 @@ const keys = {
       'ctrl',
       'arrowLeft',
       'arrowDown',
-      'arrowRight'
-    ]
-  ]
+      'arrowRight',
+    ],
+  ],
+  engShift: [
+    [
+      '~',
+      '!',
+      '@',
+      '#',
+      '$',
+      '%',
+      '^',
+      '&',
+      '*',
+      '(',
+      ')',
+      '_',
+      '+',
+      'bacspace',
+    ],
+    [
+      'tab',
+      'Q',
+      'W',
+      'E',
+      'R',
+      'T',
+      'Y',
+      'U',
+      'I',
+      'O',
+      'P',
+      '{',
+      '}',
+      '|',
+      'delete',
+    ],
+    [
+      'capslock',
+      'A',
+      'S',
+      'D',
+      'F',
+      'G',
+      'H',
+      'J',
+      'K',
+      'L',
+      ':',
+      '"',
+      'enter',
+    ],
+    [
+      'shift',
+      'Z',
+      'X',
+      'C',
+      'V',
+      'B',
+      'N',
+      'M',
+      '<',
+      '>',
+      '?',
+      'arrowUp',
+      'shift',
+    ],
+    [
+      'ctrl',
+      'win',
+      'alt',
+      'space',
+      'alt',
+      'ctrl',
+      'arrowLeft',
+      'arrowDown',
+      'arrowRight',
+    ],
+  ],
+  ru: [
+    [
+      'ё',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '0',
+      '-',
+      '=',
+      'backspace',
+    ],
+    [
+      'tab',
+      'й',
+      'ц',
+      'у',
+      'к',
+      'е',
+      'н',
+      'г',
+      'ш',
+      'щ',
+      'з',
+      'х',
+      'ъ',
+      '\\',
+      'delete',
+    ],
+    [
+      'capslock',
+      'ф',
+      'ы',
+      'в',
+      'а',
+      'п',
+      'р',
+      'о',
+      'л',
+      'д',
+      'ж',
+      'э',
+      'enter',
+    ],
+    [
+      'shift',
+      'я',
+      'ч',
+      'с',
+      'м',
+      'и',
+      'т',
+      'ь',
+      'б',
+      'ю',
+      '.',
+      'arrowUp',
+      'shift',
+    ],
+    [
+      'ctrl',
+      'win',
+      'alt',
+      'space',
+      'alt',
+      'ctrl',
+      'arrowLeft',
+      'arrowDown',
+      'arrowRight',
+    ],
+  ],
+  ruShift: [
+    ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '+', 'backspase'],
+    [
+      'tab',
+      'Й',
+      'Ц',
+      'У',
+      'К',
+      'Е',
+      'Н',
+      'Г',
+      'Ш',
+      'Щ',
+      'З',
+      'Х',
+      'Ъ',
+      '/',
+      'delete',
+    ],
+    [
+      'capslock',
+      'Ф',
+      'Ы',
+      'В',
+      'А',
+      'П',
+      'Р',
+      'О',
+      'Л',
+      'Д',
+      'Ж',
+      'Э',
+      'enter',
+    ],
+    [
+      'shift',
+      'Я',
+      'Ч',
+      'С',
+      'М',
+      'И',
+      'Т',
+      'Ь',
+      'Б',
+      'Ю',
+      ',',
+      'arrowUp',
+      'shift',
+    ],
+    [
+      'ctrl',
+      'win',
+      'alt',
+      'space',
+      'alt',
+      'ctrl',
+      'arrowLeft',
+      'arrowDown',
+      'arrowRight',
+    ],
+  ],
 };
 
 !localStorage.language ? (localStorage.language = 'eng') : null;
 
 function createKeyboard(language) {
-  keys[language].forEach(element => {
+  if (document.querySelector('.keyboard')) {
+    document.querySelector('.keyboard').remove();
+  }
+  const keyboard = document.createElement('div');
+
+  keyboard.classList.add('keyboard');
+  keys[language].forEach((element) => {
     const lineOfkeys = document.createElement('div');
     lineOfkeys.classList.add('lineOfkeys');
     keyboard.appendChild(lineOfkeys);
-    element.forEach(key => {
-      console.log(key);
+    element.forEach((key) => {
+      // console.log(key);
       lineOfkeys.append(createButtons(key));
     });
   });
+  body.appendChild(keyboard);
 }
 
 function createButtons(key) {
@@ -107,4 +324,48 @@ function createButtons(key) {
   return button;
 }
 
+let shiftFlag = false;
+let altLeft = false;
+
+function keyPress(event) {
+  if (event.code === 'ShiftLeft') {
+    shiftFlag = true;
+  }
+
+  if (event.code === 'AltLeft') {
+    altLeft = true;
+  }
+
+  if (
+    (event.code === 'AltLeft' && shiftFlag === true) ||
+    (event.code === 'ShiftLeft' && altLeft === true)
+  ) {
+    console.log('ru');
+    if (localStorage.language === 'eng') {
+      localStorage.language = 'ru';
+
+      createKeyboard(localStorage.language);
+    } else {
+      localStorage.language = 'eng';
+
+      createKeyboard(localStorage.language);
+    }
+  }
+}
+function keyUp(event) {
+  if (event.key === 'Shift' && event.key === 'Alt') {
+    if (localStorage.language === 'eng') {
+      localStorage.language = 'ru';
+      createKeyboard(localStorage.language);
+    } else {
+      localStorage.language = 'eng';
+      createKeyboard(localStorage.language);
+    }
+  }
+}
+
 createKeyboard(localStorage.language);
+
+document.addEventListener('keydown', keyPress);
+
+document.addEventListener('keyUp', keyUp);
